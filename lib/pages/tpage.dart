2f32395729a1f5fdebc02b2a9cdf40577b71b498
WillyOB45 <chukswilly45@gmail.com>
2024-01-11 13:10:10 +0100

    slide bar

--- lib/pages/tpage.dart
+++ lib/pages/tpage.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_slidable/flutter_slidable.dart';
 
 class tpage extends StatefulWidget {
   const tpage({super.key});
@@ -13,53 +12,7 @@ class _tpageState extends State<tpage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Center(
-          child: Slidable(
-        endActionPane: ActionPane(motion: StretchMotion(), children: [
-          SlidableAction(
-            onPressed: (context) {},
-            icon: Icons.share,
-            label: "s h a r e",
-            backgroundColor: Colors.pinkAccent.shade100,
-          ),
-          SlidableAction(
-            onPressed: (context) {},
-            icon: Icons.message,
-            label: "s e n d",
-            backgroundColor: Colors.pinkAccent.shade700,
-          ),
-        ]),
-        startActionPane: ActionPane(
-          motion: BehindMotion(),
-          children: [
-            SlidableAction(
-              onPressed: (context) {},
-              icon: Icons.phone,
-              label: "p h o n e ",
-              backgroundColor: Colors.pinkAccent.shade400,
-            ),
-            SlidableAction(
-              onPressed: (context) {},
-              icon: Icons.message,
-              label: "c h a t ",
-              backgroundColor: Colors.pinkAccent.shade200,
-            ),
-          ],
-        ),
-        child: Container(
-          color: Colors.deepPurple,
-          child: ListTile(
-              leading: Icon(Icons.person),
-              title: Text(
-                "C h u k s",
-                style: TextStyle(color: Colors.white),
-              ),
-              subtitle: Text(
-                "u s e r",
-                style: TextStyle(color: Colors.white),
-              )),
-        ),
-      )),
+      body: Center(),
     );
   }
 }
